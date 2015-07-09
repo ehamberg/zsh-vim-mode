ac9796a5180f3491383f7436d2704c270960449f
Erlend Hamberg <erlend@hamberg.no>
2015-07-09 16:51:34 +0200

    Add text for delimiters and brackets
    From <https://github.com/zsh-users/zsh/commit/d257f01>.

--- vimmode.plugin.zsh
+++ vimmode.plugin.zsh
@@ -17,6 +17,24 @@ bindkey "^[[B" down-line-or-history
 bindkey "^[[C" vi-forward-char
 bindkey "^[[D" vi-backward-char
 
+# Text object for matching characters between a particular delimiter
+autoload -U select-quoted
+zle -N select-quoted
+for m in visual viopp; do
+    for c in {a,i}{\',\",\`}; do
+        bindkey -M $m $c select-quoted
+    done
+done
+
+# Text objects for matching characters between matching pairs of brackets
+autoload -U select-bracketed
+zle -N select-bracketed
+for m in visual viopp; do
+    for c in {a,i}${(s..)^:-'()[]{}<>bB'}; do
+        bindkey -M $m $c select-bracketed
+    done
+done
+
 # Surround commands
 autoload -Uz surround
 zle -N delete-surround surround
